423033663d2b2c00f5af766d1f647e00c87140c3
Bernhard Berger <bernhardberger3456@gmail.com>
2024-04-29 08:47:53 +0200

    Use pages

--- website/src/parsers/djot/index.js
+++
@@ -1,6 +0,0 @@
-import 'codemirror/mode/markdown/markdown';
-
-export const id = 'djot';
-export const displayName = 'Djot';
-export const mimeTypes = ['text/djot'];
-export const fileExtension = 'dj';
